e5574632955c9ddd5931d510fa5bf3a1b2e9c1c9
resetmerlin <resetmerlin@gmail.com>
2024-03-01 16:48:03 +0900

    Solved Question 2864

--- 2864. Maximum Odd Binary Number/index.js
+++ 2864. Maximum Odd Binary Number/index.js
@@ -5,6 +5,8 @@
 var maximumOddBinaryNumber = function (s) {
   let arr = [...s].sort();
 
+  if (!arr.includes("0")) return arr.join("");
+
   while (arr[arr.length - 2] === "1") {
     arr.unshift(arr.pop());
   }
