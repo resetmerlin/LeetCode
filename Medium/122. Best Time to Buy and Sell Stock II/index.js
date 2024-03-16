4e5b7407226813d46eacad2fd83fcef5c8facef9
resetmerlin <resetmerlin@gmail.com>
2024-03-16 19:02:31 +0900

    Solved Question 122

--- Medium/122. Best Time to Buy and Sell Stock II/index.js
+++ Medium/122. Best Time to Buy and Sell Stock II/index.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let profit = 0;
-  for (let i = 0; i < prices.length; i++) {
-    let prev = prices[i - 1];
-    let current = prices[i];
-
-    if (prev < current) {
-      profit += current - prev;
+    let totalProfit = 0;
+    let max = 0;
+    for (let i = 0; i < prices.length; i++) {
+      let prev = prices[i-1];
+      let current = prices[i];
+  
+      if (prev < current) totalProfit += current - prev;
     }
-  }
-  return profit;
-};
+  
+    return totalProfit;
+  };
+  
